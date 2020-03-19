ec1249182105b7cb59dbc827d1c01e4e27d52dc9
Bibarius <verastuk.vasil@yandex.ru>
2020-03-19 18:36:49 +0700

    add move and board serialization

--- frontend/script.js
+++ frontend/script.js
@@ -6,15 +6,38 @@ function setClickListeners(){
 }
 
 function onClick(event){
-    path = event.path;
-    console.log(path)
+    let arr = []
+    tds = document.getElementsByTagName('td')
+    for(var i = 0; i < 9; i++){
+        symbol = tds[i].innerText
+        if(symbol == 'X'){
+            arr[i] = 1;
+        }
+        else if(symbol == 'O'){
+            arr[i] = 2
+        }
+        else{
+            arr[i] = 0
+        }
+    }
+    move = event.path[0].id;
+
+    let data = {
+        field: arr,
+        move: move
+    }
+
+    console.log(JSON.stringify(data));
+    
+
+
+
     request = new XMLHttpRequest();
-    url = 'main.py?salamaleukym=True';
+    url = 'ain.py';
     request.open('GET', url);
     request.setRequestHeader('Content-Type', 'application/x-www-form-url');
     request.send();
-}
-
+}   
 
 window.onload = function(){
     setClickListeners();
