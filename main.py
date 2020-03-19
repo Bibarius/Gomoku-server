f14c5b9c3c820252414be23e7bbf2a1d903bff24
Bibarius <verastuk.vasil@yandex.ru>
2020-03-20 00:35:17 +0700

    handling get requests

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 from http.server import *
+import json
+from urllib.parse import parse_qs
 import os
+import game
 
 class RequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         path = self.path[1:]
         if not path:
-            f = open("index.html", "rb")
+            f = open("frontend/index.html", "rb")
             File = f.read()
             self.send_response(200)
             self.send_header('Content-Lenth', str(len(File)))
             self.end_headers()
             self.wfile.write(File)
 
+        elif path[:5] == 'solve':
+            data = parse_qs(self.path)['/solve*?data'][0]
+            jdata = json.loads(data)
+            field = jdata['field']
+            move = jdata['move']
+
+            print('field = ', field)
+            print('move = ', move)
+
+
+            #!TODO ЗДЕСЬ БУДЕТ ИДТИ РЕШЕНИЕ ДОСКИ И ОТПРАВКА НАЗАД
+            
+
+
         else:
             try:                
-                f = open(path, 'rb')
+                f = open('frontend/' + path, 'rb')
             except FileNotFoundError:
+                print(path)
                 print('requested file ' + path + ' wasnt found on server')
                 return 1
             
@@ -34,8 +52,8 @@ class RequestHandler(BaseHTTPRequestHandler):
 
 
 
+
 if __name__ == "__main__":
-    os.chdir(os.getcwd() + '/frontend')
     serverAddress = ('', 8080)
     server = HTTPServer(serverAddress, RequestHandler)
     server.serve_forever()
